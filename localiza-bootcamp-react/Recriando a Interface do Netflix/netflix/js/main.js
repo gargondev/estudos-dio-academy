c5aa12d21c51f953871646f2bad0c903bedf197a
heldeveloper <heliezer.developer@gmail.com>
2021-07-24 20:51:31 -0300

    Finalizando Interface Net Flix

--- localiza-bootcamp-react/Recriando a Interface do Netflix/netflix/js/main.js
+++ localiza-bootcamp-react/Recriando a Interface do Netflix/netflix/js/main.js
@@ -1,12 +1,59 @@
+function setTheme(name) {
+  document.querySelector(`.${name}`).onclick = () => {
+    root(name);
+  };
+}
 
-        new Splide( '.splide', {
-            type   : 'loop',
-            padding: {
-                right: '20rem',
-                left : '0',
-            },
-            autoWidth: true,
-            focus    : 'lefth',
-            perPage  : 5,
-            trimSpace: false,
-        } ).mount();
+setTheme("black");
+setTheme("yellow");
+setTheme("blue");
+
+function root(name) {
+  let bodyColor = document.querySelector('body')
+  let logo = document.querySelector('.logo');
+
+  console.log(name);
+
+  if (name == "black") {
+    bodyColor.style.setProperty('background-color', 'var(--black)');
+    logo.style.setProperty("color", `var(--vermelho)`);
+    changeA("var(--gold)")
+    return false;
+     
+    
+  } else if (name == "yellow") {
+    bodyColor.style.setProperty('background-color', 'var(--yellow)');
+    logo.style.setProperty("color", `var(--black)`);
+    changeA("var(--black)")
+    return false;
+    
+  } else 
+    bodyColor.style.setProperty('background-color', 'var(--blue)');
+    logo.style.setProperty("color", `var(--amazonWhite)`);
+    changeA("var(--amazonWhite)");
+    return false;
+   
+    
+}
+
+function changeA(color){
+    let a = document.querySelectorAll('a');
+
+
+    for (let index = 0; index < a.length; index++) {
+        
+        a[index].style.setProperty("color", `${color}`);
+    }
+}
+
+new Splide(".splide", {
+  type: "loop",
+  padding: {
+    right: "20rem",
+    left: "0",
+  },
+  autoWidth: true,
+  focus: "lefth",
+  perPage: 5,
+  trimSpace: false,
+}).mount();
