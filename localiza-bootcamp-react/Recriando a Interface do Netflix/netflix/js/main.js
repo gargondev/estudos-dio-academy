894ef7748bf3773d7badae9e1262fbe0e327ab8f
heldeveloper <heliezer.developer@gmail.com>
2021-07-24 01:55:10 -0300

    Implementato estrutura de HTML e CSS Clone NetFlix

---
+++ localiza-bootcamp-react/Recriando a Interface do Netflix/netflix/js/main.js
@@ -0,0 +1,12 @@
+
+        new Splide( '.splide', {
+            type   : 'loop',
+            padding: {
+                right: '20rem',
+                left : '0',
+            },
+            autoWidth: true,
+            focus    : 'lefth',
+            perPage  : 5,
+            trimSpace: false,
+        } ).mount();
